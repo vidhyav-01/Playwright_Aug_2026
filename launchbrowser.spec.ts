536c61484bf740c6681bfbcc1c934d41e09c2c9d
vidhyav-01 <vidhupn2829@gmail.com>
2026-08-29 12:41:15 +0530

    Delete launchbrowser.spec.ts

--- launchbrowser.spec.ts
+++
@@ -1,7 +0,0 @@
-import { chromium, test } from "@playwright/test";
-test("To launch a browser", async () => {
-const browserInstance = await chromium.launch({ headless: false, channel: "msedge" });
-const browserContext = await browserInstance.newContext();
-const page = await browserContext.newPage();
-await page.goto("https://www.amazon.com/");
-});
